b4fcb43837ed7afd16d135a1ccd4ffca7c590c81
whCloudht <liudan11@chinatelecom.cn>
2023-11-16 11:44:16 +0800

    测试代码

---
+++ api_test/test1.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+base_url = "http://127.0.0.1:8002/"
+
+
+def api_test(my_api: str, method: str, data: dict):
+    url = base_url + my_api
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8"
+    }
+    if method == "POST":
+        result = requests.post(url, data=json.dumps(data), headers=headers, timeout=3000)
+        print(result)
+    else:
+        return
+
+
+if __name__ == "__main__":
+    data = {
+        "user": "test",
+        "passwd": "123456"
+    }
+    api_test("api/getToken", "POST", data)
+
